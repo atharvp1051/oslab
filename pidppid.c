9bb5231e426c1182c202fb07ad824115846cf40b
eng19cs0052-atharva <76119710+eng19cs0052-atharva@users.noreply.github.com>
2021-11-28 21:32:24 +0530

    Create pidppid.c

---
+++ pidppid.c
@@ -0,0 +1,25 @@
+
+#include<stdio.h>
+#include<sys/types.h>
+#include<unistd.h>
+#include<sys/wait.h>
+void main()
+{
+  int pid;
+  pid=fork();
+  if (!pid)
+  {
+    printf("child process..");
+    printf("\n\n Child PID :%d",getpid());
+    printf("\nParent PID :%d",getppid());
+    printf("\n\nFinished with child\n");
+  }
+  
+  else
+  {
+   wait(NULL);
+    printf("\nParent process");
+    printf("\nParent PID :%d",getpid());
+    printf("\nchild PID:%d",pid);
+    
+  }
